5d871d0375e54e8b438a64956bf9511322df11ad
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-09 10:06:24 +0530

    Add

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures");
+
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
